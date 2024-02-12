460c95e56cb8c91af790ed9bf82dc7f2b7422233
Lawrence Narh-Lawson <lawrence.narh-lawson@boomin.com>
2024-02-12 16:28:26 +0000

    added route folders

---
+++ src/routes/contact/index.tsx
@@ -0,0 +1,17 @@
+import { component$ } from '@builder.io/qwik';
+
+export default component$(() => {
+  return (
+    <article>
+      <h2>About</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quibusdam
+        consequatur accusantium, explicabo esse, deserunt illo, soluta quia quas
+        illum modi accusamus voluptates porro! Officia voluptas ducimus sit
+        labore a blanditiis unde? Alias fugiat quidem excepturi ab labore hic
+        odit? Minima corrupti error tempora ipsa? Enim modi tempora eveniet
+        harum voluptas.
+      </p>
+    </article>
+  );
+});
